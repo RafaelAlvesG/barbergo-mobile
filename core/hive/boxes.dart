0b1412c80ff58004a9299eb9d0e41e6e2e48312d
Rafael Alves <rafaelalvesgarcia01@gmail.com>
2026-07-08 00:23:21 -0300

    Delete core directory

--- core/hive/boxes.dart
+++
@@ -1,8 +0,0 @@
-class AppBoxes {
-  static const String barbearias = 'barbearias';
-  static const String profissionais = 'profissionais';
-  static const String agendamentos = 'agendamentos';
-  static const String funcionario = 'funcionario';
-  static const String clientes = 'clientes';
-  static const String settings = 'settings';
-}
